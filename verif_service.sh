8df34e0ae9d05845592d9adfffe072f4caf59718
Massil <massilbraik0@gmail.com>
2024-11-18 09:37:48 +0100

    modif

--- verif_service.sh
+++ verif_service.sh
@@ -6,32 +6,27 @@ if [ -z "$service_name" ]; then
     exit 8
 fi 
 
-if ! systemctl list-units --type=service --all | grep -q "$service_name"; then
-    
+
+if ! systemctl cat "$service_name" > /dev/null 2>&1; then
+    echo "Erreur : le service '$service_name' n'existe pas."
     exit 7
 fi
 
 
 service_status=$(systemctl is-active "$service_name")
 
-
 if [ "$service_status" = "active" ]; then
     exit 0
 elif [ "$service_status" = "inactive" ]; then
     exit 1
 elif [ "$service_status" = "failed" ]; then
-    
     exit 2
 elif [ "$service_status" = "activating" ]; then
-    
     exit 3
 elif [ "$service_status" = "deactivating" ]; then
-    
     exit 4
 elif [ "$service_status" = "unknown" ]; then
-    
     exit 5
 else
-    
     exit 6
 fi
